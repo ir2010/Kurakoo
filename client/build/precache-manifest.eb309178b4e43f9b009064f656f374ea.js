ee02e0614508a13c5a8c40da904f31bd9be5c578
ishuraj2010@gmail.com <ishuraj2010@gmail.com>
2021-03-23 14:55:58 +0530

    minor changes

---
+++ client/build/precache-manifest.eb309178b4e43f9b009064f656f374ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5be1de87cac036224db557cec5d2d44a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "27b19c9ec4c4bb5759a6",
+    "url": "./static/css/2.de5665f9.chunk.css"
+  },
+  {
+    "revision": "9cdf889cc916a8636e88",
+    "url": "./static/css/main.4076c996.chunk.css"
+  },
+  {
+    "revision": "27b19c9ec4c4bb5759a6",
+    "url": "./static/js/2.cdebf089.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "./static/js/2.cdebf089.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cdf889cc916a8636e88",
+    "url": "./static/js/main.920a4447.chunk.js"
+  },
+  {
+    "revision": "d90c5b93796701d5570a",
+    "url": "./static/js/runtime-main.8b0ae790.js"
+  }
+]);
